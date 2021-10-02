a859fe8016048d2508d473047e524669bb2806b3
balaji-kss <kss.balaji1@gmail.com>
2021-10-02 16:42:06 -0400

    added new asssignment

--- HPC/Homework2_BalajiSundareshan/diningb_mutex.cpp
+++ HPC/Homework2_BalajiSundareshan/diningb_mutex.cpp
@@ -3,15 +3,21 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <cassert>
+# include <string>
+
+using namespace std;
 
 struct philosopher_t {
     int position;
     int count;
-    float total_think_duration;
-    float total_eat_duration;
-    float total_wait_duration;
-    pthread_mutex_t *left_fork;
-    pthread_mutex_t *right_fork;
+    int current_think_duration = 0;
+    int current_eat_duration = 0;
+    int current_wait_duration = 0;
+    int total_think_duration = 0;
+    int total_eat_duration = 0;
+    int total_wait_duration = 0;
+    pthread_mutex_t left_fork;
+    pthread_mutex_t right_fork;
 };
 
 int get_random_number(int min, int max);
@@ -21,6 +27,7 @@ void eat(philosopher_t *philosopher, int max_eat_time);
 void *start_activity_philosopher(void *arg);
 int is_last_philosopher(philosopher_t *philosopher);
 void create_forks(pthread_mutex_t *forks, int num_philosophers);
+bool take_forks(philosopher_t *philosopher, pthread_mutex_t left_fork, pthread_mutex_t right_fork);
 
 void start_threads(pthread_t *threads, pthread_mutex_t *forks, int num_philosophers)
 {
@@ -28,15 +35,11 @@ void start_threads(pthread_t *threads, pthread_mutex_t *forks, int num_philosoph
     for(int i = 0; i < num_philosophers; i++) {
 
         philosopher_t *philosopher = new philosopher_t;
-        printf("phil init\n");
         philosopher->position = i;
-        printf("phil init pos\n");
+        printf("phil init %d\n", i);
         philosopher->count = num_philosophers;
-        philosopher->total_think_duration = 0.0;
-        philosopher->total_eat_duration = 0.0;
-        philosopher->total_wait_duration = 0.0;
-        philosopher->left_fork = &forks[i];
-        philosopher->right_fork = &forks[(i + 1) % num_philosophers];
+        philosopher->left_fork = forks[i];
+        philosopher->right_fork = forks[i + 1 % num_philosophers];
         pthread_create(&threads[i], NULL, start_activity_philosopher, (void *)philosopher);
   }
 }
@@ -44,55 +47,79 @@ void start_threads(pthread_t *threads, pthread_mutex_t *forks, int num_philosoph
 void think(philosopher_t *philosopher, int max_think_time)
 {
 
-    int philosopher_id = philosopher->position + 1;
-    int think_time = get_random_number(1, max_think_time);
-    philosopher->total_think_duration += think_time;
+    philosopher->current_think_duration = get_random_number(1, max_think_time);
 
-    printf("Philosopher %d started thinking\n", philosopher_id);
+    printf("Philosopher %d think time: %d \n", philosopher->position, philosopher->current_think_duration);
+    philosopher->total_think_duration += philosopher->current_think_duration;
+
+    printf("Philosopher %d started thinking\n", philosopher->position);
     // sleep
-    sleep(think_time);
+    sleep(philosopher->current_think_duration);
 
-    printf("Philosopher %d stopped thinking after %f\n", (philosopher_id, think_time));
+    printf("Philosopher %d stopped thinking after %d\n", philosopher->position, philosopher->current_think_duration);
 
 }
 
 void eat(philosopher_t *philosopher, int max_eat_time)
 {
 
-    int philosopher_id = philosopher->position + 1;
-    int eat_time = get_random_number(1, max_eat_time);
-    philosopher->total_eat_duration += eat_time;
+    philosopher->current_eat_duration = get_random_number(1, max_eat_time);
+    philosopher->total_eat_duration += philosopher->current_eat_duration;
 
-    printf("Philosopher %d started eating\n", philosopher_id);
+    printf("Philosopher %d started eating\n", philosopher->position);
     // sleep
-    sleep(eat_time);
+    sleep(philosopher->current_eat_duration);
 
-    printf("Philosopher %d stopped eating after %f\n", (philosopher_id, eat_time));
+    printf("Philosopher %d stopped eating after %d \n", philosopher->position, philosopher->current_eat_duration);
 
 }
 
-void take_forks(philosopher_t *philosopher)
+// bool take_forks(philosopher_t *philosopher, pthread_mutex_t left_fork, pthread_mutex_t right_fork)
+// {
+
+//   if (pthread_mutex_trylock (&left_fork) == 0){
+  
+//     printf("Philosopher %d got the left fork\n ", philosopher->position);
+
+//     if (pthread_mutex_trylock (&right_fork) == 0){
+
+//       printf("Philosopher %d got the right fork\n ", philosopher->position);
+
+//       return true;
+
+//     } else {
+
+//       pthread_mutex_unlock (&left_fork);
+
+//     }
+//   } 
+//   return false;
+// }
+
+bool take_forks(philosopher_t *philosopher, pthread_mutex_t left_fork, pthread_mutex_t right_fork)
 {
-  if (is_last_philosopher(philosopher))
-  {
-    printf("right Philosopher %d lock\n", philosopher->position + 1);
-    pthread_mutex_lock (philosopher->right_fork);
-    pthread_mutex_lock (philosopher->left_fork);
-    printf("right Philosopher %d lock END\n", philosopher->position + 1);
+
+  int status = pthread_mutex_trylock (&left_fork);
+  if (status == 0){
+    printf("left locked\n");
   }
-  else
-  {
-    printf("left Philosopher %d lock\n", philosopher->position + 1);
-    pthread_mutex_lock(philosopher->left_fork);
-    pthread_mutex_lock(philosopher->right_fork);
+  int statusa = pthread_mutex_trylock (&right_fork);
+  if (statusa == 0){
+    printf("right locked\n");
   }
+
+  if (status == 0 && statusa == 0){
+    return true;
+  }
+
+  return false;
 }
 
 void place_forks(philosopher_t *philosopher)
 {
-    printf("Philosopher %d unlock\n", philosopher->position + 1);
-    pthread_mutex_unlock(philosopher->right_fork);
-    pthread_mutex_unlock(philosopher->left_fork);
+    printf("Philosopher %d unlock\n", philosopher->position);
+    pthread_mutex_unlock(&philosopher->right_fork);
+    pthread_mutex_unlock(&philosopher->left_fork);
 }
 
 int is_last_philosopher(philosopher_t *philosopher)
@@ -115,15 +142,23 @@ int get_random_number(int min, int max)
 void *start_activity_philosopher(void *arg)
 {
     philosopher_t *philosopher = (philosopher_t *) arg;
+    bool status;
 
     while(true)
     {
         think(philosopher, 2);
-        take_forks(philosopher);
+        if (is_last_philosopher(philosopher))
+        {
+          status = take_forks(philosopher, philosopher->right_fork, philosopher->left_fork);  
+        } else {
+          printf("not last: %d\n", philosopher->position);
+          status = take_forks(philosopher, philosopher->left_fork, philosopher->right_fork);  
+        }
+        
+        printf("Both Fork status: %d\n", status);
         eat(philosopher, 2);
         place_forks(philosopher);
     }
-
 }
 
 void create_forks(pthread_mutex_t *forks, int num_philosophers)
