9145b3a17766d7ec2233e60bab623aa7414d202b
balaji-kss <kss.balaji1@gmail.com>
2021-10-31 16:26:57 -0400

    add sample code

--- HPC/Homework3_BalajiSundareshan/Question1/sample.cpp
+++ HPC/Homework3_BalajiSundareshan/Question1/sample.cpp
@@ -14,10 +14,18 @@ int main() {
       vect3[i] = 0.0;
     }
 
+    struct timespec start, end;
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
     for (size_t i=0; i < VECTOR_SIZE; i++) {
       vect3[i] = vect1[i] + vect2[i];
     }
 
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double time_taken = (end.tv_sec - start.tv_sec);
+    time_taken += (end.tv_nsec - start.tv_nsec) / 1000000000.0;
+    cout << "time_taken: " << time_taken << endl; 
     cout << "Vect 3: " << vect3[VECTOR_SIZE - 1] << endl;
     return 0;
 }
