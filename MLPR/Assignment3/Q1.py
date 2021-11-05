53a1c69ef132c8983821403d330e613ee04806b6
balaji-kss <kss.balaji1@gmail.com>
2021-11-05 11:13:29 -0400

    added gen data

--- MLPR/Assignment3/Q1.py
+++ MLPR/Assignment3/Q1.py
@@ -24,23 +24,31 @@ def set_mean_cov():
 def gen_class_samples(num_samples):
 
     class_dist = np.random.randint(num_labels, size=num_samples)
-    class_samples = [np.sum(class_dist==label).astype('int') for label in labels]
+    class_samples = [np.sum(class_dist==label_id).astype('int') for label_id in label_ids]
 
     return class_samples
 
 def generate_data_pxgl(priors, means, covs, num_samples):
-
-    m0, m1 = means
-    C0, C1 = covs
     
     class_samples = gen_class_samples(num_samples)
     print('class_samples: ',class_samples)
 
     # generate class data
-    for label in clas
-    pxgl1 = np.random.multivariate_normal(m1, C1, N1).T
+    pxgls = np.array([], dtype=np.float).reshape(3,0)
+    labels = []
+    for label_id in label_ids:
+        num_cls_samples = class_samples[label_id]
+        mean = means[label_id]
+        cov = covs[label_id]
+        pxgl = np.random.multivariate_normal(mean, cov, num_cls_samples).T
+        pxgls = np.concatenate((pxgls, pxgl), axis=1)
+        class_label = [label_id]*num_cls_samples
+        labels += class_label
     
-    return data, N0, N1
+    labels = np.array(labels).reshape((1, -1))
+    data = np.concatenate((pxgls, labels), axis=0)
+
+    return data, class_samples
 
 def generate_data_pxgl_samples(samples_type):
 
@@ -49,30 +57,64 @@ def generate_data_pxgl_samples(samples_type):
         sample_type = samples_type[key]
         num_samples = int(sample_type[0][0])
 
-        data_wt_labels, N0, N1 = generate_data_pxgl(pL, [m0, m1], [C0, C1], num_samples)
-
-        sample_type[1] = [N0, N1]
+        data_wt_labels, cls_samples = generate_data_pxgl(priors, means, covs, num_samples)
+        sample_type[1] = cls_samples
         sample_type[2] = data_wt_labels
 
-        label_names = ["True label distribution for " + str(num_samples) + " for two classes", "x1", "x2"]
+        label_names = ["True label distribution for " + str(num_samples) + " for two classes", "x", "y", "z"]
         plot_dist(data_wt_labels, label_names)
 
     return samples_type
 
+def split_data(data_wt_labels):
+
+    samples = []
+
+    for label_id in label_ids:
+        class_ids = np.where(data_wt_labels[-1,:]==label_id)[0]
+        cls_samples = data_wt_labels[:,class_ids]    
+        samples.append(cls_samples)
+
+    return samples
+
+def plot_dist(data, label_names):
+
+    tname, xname, yname, zname = label_names
+
+    print('***** plot *****')
+    samples = split_data(data)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    colors = ['red', 'blue', 'green', 'brown']
+    for label_id, sample in enumerate(samples):
+        ax.scatter(sample[0, :], sample[1, :], sample[2, :], s=5, color = colors[label_id], label = 'class ' + str(label_id), marker='*')
+    
+    ax.set_title(tname)
+    ax.set_xlabel(xname)
+    ax.set_ylabel(yname)
+    ax.set_zlabel(zname)
+    plt.legend()
+    plt.show()
+
 if __name__ == "__main__":
 
     dim = 3
-    labels = [0, 1, 2, 3]
-    num_labels = len(labels)
+    label_ids = [0, 1, 2, 3]
+    num_labels = len(label_ids)
     priors = [0.25, 0.25, 0.25, 0.25]
 
     samples_type = {
-        'D100': [[100]],  
-        'D200': [[200]],
-        'D500': [[500]],
-        'D1000': [[1000]],
-        'D2000': [[2000]],
-        'D5000': [[5000]],
+        'D100': [[100], [], []],  
+        'D200': [[200], [], []],
+        'D500': [[500], [], []],
+        'D1k': [[1000], [], []],
+        'D2k': [[2000], [], []],
+        'D5k': [[5000], [], []],
+        'D100k': [[100000], [], []],
     }
 
     means, covs = set_mean_cov()
+
+    generate_data_pxgl_samples(samples_type)
