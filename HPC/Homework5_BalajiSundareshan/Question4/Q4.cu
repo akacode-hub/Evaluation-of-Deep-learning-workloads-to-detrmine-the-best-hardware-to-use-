51c2f85b373cd14c24968de71b58191e0a55b3a5
balajisundar <kss.balaji1@gmail.com>
2021-11-28 15:55:59 -0500

    Q4 - working - need to reduce array size

--- HPC/Homework5_BalajiSundareshan/Question4/Q4.cu
+++ HPC/Homework5_BalajiSundareshan/Question4/Q4.cu
@@ -5,9 +5,12 @@
 #include <cuda.h>
 #include <curand_kernel.h>
 
-double PI25DT = 3.141592653589793238462643;         /* 25-digit-PI*/
+const double PI25DT = 3.141592653589793238462643;         /* 25-digit-PI*/
+const int iteration = 2;
+const int num_blocks = 1000;
+const int num_threads_per_block = 256;
 
-__global__ void fill_dart_count(int *in_dart_counts, int *num_darts_thread)
+__global__ void fill_dart_count(int *in_dart_counts)
 {
     int tid = blockIdx.x*blockDim.x + threadIdx.x;
 
@@ -17,7 +20,10 @@ __global__ void fill_dart_count(int *in_dart_counts, int *num_darts_thread)
     curandState_t rng;
 	curand_init(clock64(), tid, 0, &rng);
 
-    for (toss = 0; toss < *num_darts_thread; toss++) {
+    //printf("num_darts_thread: %d\n", iteration);
+    num_toss_in = 0;
+    for (toss = 0; toss < iteration; toss++) {
+
 		x = curand_uniform(&rng); // Random x position in [0,1]
 		y = curand_uniform(&rng); // Random y position in [0,1]
 
@@ -25,18 +31,20 @@ __global__ void fill_dart_count(int *in_dart_counts, int *num_darts_thread)
             num_toss_in += 1;
         }
     }
-
+    
     in_dart_counts[tid] = num_toss_in;
 
 }
 
-__global__ void count_darts(int *in_dart_counts, long *num_darts, int *size)
+__global__ void count_darts(int *in_dart_counts, long *num_darts)
 {
     int i;
     long sum = 0.0;
-    for(i = 0; i<*size; i++){
+    for(i = 0; i<num_blocks*num_threads_per_block; i++){
+        // printf("in_dart_counts %d: %d\n",i,in_dart_counts[i]);
         sum += in_dart_counts[i];
     }
+    // printf("sum: %ld\n",sum);
     *num_darts = sum;    
 }
 
@@ -48,8 +56,6 @@ int main(int argc, char *argv[])
 
     assert(("./Q1 <number of darts>", argc == 2));
     long num_darts = atoi(argv[1]);
-    int num_blocks = 1000;
-    int num_threads_per_block = 256;
     assert(("num_darts should be divisible by 256*1000", num_darts % num_blocks * num_threads_per_block == 0));
 
     int num_dart_per_thread = num_darts / (num_blocks * num_threads_per_block);
@@ -62,9 +68,7 @@ int main(int argc, char *argv[])
     clock_gettime(CLOCK_MONOTONIC, &start);
 
     int *dart_counts_block;
-    int *size_gpu, size;
     cudaMalloc((void**)&dart_counts_block, num_blocks * num_threads_per_block * sizeof(int));
-    cudaMalloc((void**)&size_gpu, sizeof(size));
 
     long *num_darts_gpu, num_darts_in;
     cudaMalloc((void**)&num_darts_gpu, sizeof(num_darts_in));
@@ -72,9 +76,9 @@ int main(int argc, char *argv[])
     int *num_dart_per_thread_gpu;
     cudaMalloc((void**)&num_dart_per_thread_gpu, sizeof(num_dart_per_thread));
 
-    fill_dart_count<<<num_blocks, num_threads_per_block>>>(dart_counts_block, num_dart_per_thread_gpu);
+    fill_dart_count<<<num_blocks, num_threads_per_block>>>(dart_counts_block);
 
-    count_darts<<<1, 1>>>(dart_counts_block, num_darts_gpu, size_gpu);
+    count_darts<<<1, 1>>>(dart_counts_block, num_darts_gpu);
 
     cudaMemcpy(&num_darts_in, num_darts_gpu, sizeof(num_darts_in), cudaMemcpyDeviceToHost);
 
@@ -82,9 +86,13 @@ int main(int argc, char *argv[])
     double time_elapsed = (end.tv_sec - start.tv_sec);
     time_elapsed += (end.tv_nsec - start.tv_nsec) / 1000000000.0;
 
-    long calculated_pi = (4*num_darts_in)/((double) num_darts);
+    printf("num_darts_in: %ld\n", num_darts_in);
+
+    double calculated_pi = (4*num_darts_in)/((double) num_darts);
+    printf("Calculated_pi: %.16f\n", calculated_pi);
+
     double error = fabs(calculated_pi - PI25DT);
-    printf("Elapsed time = %f seconds \n", time_elapsed);
+    printf("Elapsed time = %f seconds \n", time_elapsed);    
     printf("Calculated Pi is %.16f, Error is %.16f\n", calculated_pi, error);
 
     return 0;
--- HPC/Homework5_BalajiSundareshan/Question4/bash.sh
+++ HPC/Homework5_BalajiSundareshan/Question4/bash.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --time=1:00:00
+#SBATCH --time=0:30:00
 #SBATCH --job-name=your_job_name
 #SBATCH --mem=1G
 #SBATCH --gres=gpu:1
-#SBATCH --output=Q4_out1
+#SBATCH --output=Q4_out
 #SBATCH --partition=gpu
-#./Q4 512000
-./pi
+./Q4 256000
+# ./pi1
