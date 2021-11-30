1cad598dad93d020ad7d3dee8ac4375c117c762d
balaji sundar <kss.balaji1@gmail.com>
2021-11-30 14:08:30 -0500

    tiled WIP

--- HPC/Homework5_BalajiSundareshan/Question2/Q2.cu
+++ HPC/Homework5_BalajiSundareshan/Question2/Q2.cu
@@ -10,7 +10,7 @@
 #define MAX_NUM 100
 const int N = 4;
 const int num_threads_per_block = 2;
-const int num_blocks = 1;
+const int num_blocks = 2;
 const int sblock_size = num_threads_per_block + 2;
 
 __global__ void non_tile_compute(float b[][N][N], float a[][N][N])
@@ -36,20 +36,31 @@ __global__ void tile_compute(float b[][N][N], float a[][N][N])
     int j = blockIdx.y * blockDim.y + threadIdx.y;
     int k = blockIdx.z * blockDim.z + threadIdx.z;
 
-    int offx = threadIdx.x + num_threads_per_block;
-    int offy = threadIdx.y + num_threads_per_block;
-    int offz = threadIdx.z + num_threads_per_block;
+    int offx, offy, offz;
+    int offset = num_threads_per_block;
 
-    shared_b[threadIdx.x][threadIdx.y][threadIdx.z] = b[i][j][k];
-    printf("tidx: %d, tidy: %d, tidz: %d, sb: %f\n", threadIdx.x, threadIdx.y, threadIdx.z, shared_b[threadIdx.x][threadIdx.y][threadIdx.z]);
-
-    if (offx < sblock_size && offy < sblock_size && offz < sblock_size){
-        shared_b[offx][offy][offz] = b[i][j][k];
-        printf("offx: %d, offy: %d, offz: %d, sb: %f\n", offx, offy, offz, shared_b[offx][offy][offz]);
+    for(offx=0; offx<sblock_size; offx+=2){
+        for(offy=0; offy<sblock_size; offy+=2){
+            for(offz=0; offz<sblock_size; offz+=2){
+                shared_b[threadIdx.x + offx][threadIdx.y + offy][threadIdx.z + offz] = b[i][j][k];
+                printf("i: %d, j: %d, k: %d, ox: %d, oy: %d, oz: %d\n", i, j, k, threadIdx.x + offx, threadIdx.y + offy, threadIdx.z + offz);
+            }
+        }
     }
-        
 
     __syncthreads();
+
+    if(blockIdx.x==0 && blockIdx.y==0 && blockIdx.z==0 && threadIdx.x==0 && threadIdx.y==0 && threadIdx.z==0){
+    printf("shared: \n\n");
+    int i_, j_, k_;
+    for(i_=0;i_<sblock_size;i_++){
+        for(j_=0;j_<sblock_size;j_++){
+            for(k_=0;i_<sblock_size;k_++){
+                printf("%f, ", shared_b[i_][j_][k_]);
+                }
+            }
+        }
+    }
 
     int tx = threadIdx.x + 1;
     int ty = threadIdx.y + 1;
@@ -66,12 +77,12 @@ __global__ void tile_compute(float b[][N][N], float a[][N][N])
 void gen_mat(float arr[][N][N]){
 
     int i, j, k;
-    for(k=0;k<N;k++){
+    for(i=0;i<N;i++){
         for(j=0;j<N;j++){
-            for(i=0;i<N;i++){
-                arr[k][j][i] = rand() % MAX_NUM + MIN_NUM;
-                arr[k][j][i] = (float)rand()/(float)(RAND_MAX) * arr[k][j][i];
-                //arr[k][j][i] = 1;
+            for(k=0;k<N;k++){
+                arr[i][j][k] = rand() % MAX_NUM + MIN_NUM;
+                arr[i][j][k] = (float)rand()/(float)(RAND_MAX) * arr[i][j][k];
+                //arr[i][j][k] = 1;
             }
         }
     }
@@ -80,10 +91,10 @@ void gen_mat(float arr[][N][N]){
 void print_mat(float arr[][N][N]){
 
     int i, j, k;
-    for(k=0;k<N;k++){
+    for(i=0;i<N;i++){
         for(j=0;j<N;j++){
-            for(i=0;i<N;i++){
-                printf("%f, ", arr[k][j][i]);
+            for(k=0;k<N;k++){
+                printf("%f, ", arr[i][j][k]);
             }
         }
     }
@@ -132,9 +143,9 @@ int main(int argc, char *argv[])
     dim3 threads_per_block(num_threads_per_block, num_threads_per_block, num_threads_per_block);
     dim3 blocks(num_blocks, num_blocks, num_blocks);
 
-    non_tile_compute<<<blocks, threads_per_block>>>(reinterpret_cast<float (*)[N][N]>(b), a_vals_tile);
+    non_tile_compute<<<blocks, threads_per_block>>>(reinterpret_cast<float (*)[N][N]>(b), a_vals_nontile);
 
-    tile_compute<<<blocks, threads_per_block>>>(reinterpret_cast<float (*)[N][N]>(b), a_vals_nontile);
+    tile_compute<<<blocks, threads_per_block>>>(reinterpret_cast<float (*)[N][N]>(b), a_vals_tile);
 
     cudaDeviceSynchronize();
 
@@ -150,17 +161,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-// if (threadIdx.x==0 && threadIdx.y==0 && threadIdx.z==0){
-//     printf("shared: \n\n");
-//     int i_, j_, k_;
-//     for(k_=0;k_<sblock_size;k_++){
-//         for(j_=0;j_<sblock_size;j_++){
-//             for(i_=0;i_<sblock_size;i_++){
-//                 printf("%f, ", shared_b[k_][j_][i_]);
-//             }
-//         }
-//     }
-// }
+
 
 
 // __syncthreads();
