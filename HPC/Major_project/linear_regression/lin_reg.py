bc69964a2d952a7b331a67da01f35e00c281d2fb
balaji sundar <kss.balaji1@gmail.com>
2021-12-04 21:12:36 -0500

    lin reg CPU + GPU done

--- HPC/Major_project/linear_regression/lin_reg.py
+++ HPC/Major_project/linear_regression/lin_reg.py
@@ -110,7 +110,7 @@ def calc_mae(output, label):
 
     return torch.mean(torch.abs(output - label))
 
-def train(num_dim, gpu_id=0):
+def train_gpu(num_dim, gpu_id=0):
 
     features, labels = get_data(fpath)    
     data = train_test_split(features, labels, num_train)
@@ -175,6 +175,66 @@ def train(num_dim, gpu_id=0):
         if epoch % 2 == 0:
             torch.save(model.state_dict(), model_save_dir + str(epoch) + '.pth')
 
+def train_cpu(num_dim):
+
+    features, labels = get_data(fpath)    
+    data = train_test_split(features, labels, num_train)
+    train_data, train_labels = data[:2]
+    test_data, test_labels = data[2:]
+    scale_train_data, scale_test_data = scale_data(train_data, test_data)
+
+    train_labels, test_labels = scale_labels(train_labels, test_labels)
+
+    if use_pca:
+        scale_train_data, scale_test_data, num_dim = dimension_reduction(scale_train_data, scale_test_data)
+        print('pca dim: ', num_dim)
+
+    torch.manual_seed(0)
+    model = MLP(num_dim, pred_dim)
+    print('model: ',model)
+    
+    # define loss function (criterion) and optimizer
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=lr_steps, gamma=lr_drop)
+
+    # Data loading code
+    train_dataset = YearPredictionDataset(features=scale_train_data, labels=train_labels)
+
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               num_workers=0,
+                                               pin_memory=True)
+
+
+    total_step = len(train_loader)
+
+    for epoch in range(num_epochs):
+
+        start = time.time()
+        for i, sample in enumerate(train_loader):
+            
+            features = sample['features'].float()
+            labels = sample['label'].float()
+
+            # Forward pass
+            outputs = model(features)
+            loss = criterion(outputs, labels)
+            metric = calc_mae(outputs, labels)
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        
+        scheduler.step()
+        train_mins = (time.time() - start) / 60 
+        print('Epoch [{}/{}], time: {:.4f} mins, lr: {}, mse: {:.4f},  mae: {:.4f}'.format(epoch + 1, num_epochs, train_mins, scheduler.get_last_lr()[0], loss.item(), metric), flush=True)
+
+        if epoch % 2 == 0:
+            torch.save(model.state_dict(), model_save_dir + str(epoch) + '.pth')
+
 def validate(network, num_dim):
 
     features, labels = get_data(fpath)    
@@ -187,40 +247,60 @@ def validate(network, num_dim):
         scale_train_data, scale_test_data, num_dim = dimension_reduction(scale_train_data, scale_test_data)
 
     train_labels, test_labels = scale_labels(train_labels, test_labels)
-    
+
+    mean_mae = 0.0    
+    tot_pred_time = 0.0
     for idx in range(scale_test_data.shape[0]):
         
         test_inp = scale_test_data[idx]
         test_label = test_labels[idx]
         test_inp = torch.tensor(test_inp).float()
-        pred = network(test_inp).detach().numpy()
-        mse_err = (pred - test_label) ** 2
+
+        start = time.time()
+        pred = network(test_inp)
+        end = time.time()
+        tot_pred_time += end - start
+
+        pred = pred.detach().numpy()
+        mae_err = np.abs(pred - test_label)
 
         test_label = int(test_label + 1922)
         pred = int(pred + 1922)
 
-        print('idx: ',idx, ' label: ',test_label, ' pred: ', pred, ' mse_err: ',mse_err)
+        #print('idx: ',idx, ' label: ',test_label, ' pred: ', pred, ' mse_err: ',mse_err)
+        mean_mae += mae_err
+    
+    return np.round(mean_mae/scale_test_data.shape[0], 3)[0], np.round(tot_pred_time, 3)
 
 def test(model_path, num_dim):
 
+    if not torch.cuda.is_available():
+        usegpu = 0
+        print('Using CPU for testing')
+    else:
+        usegpu = 1
+        print('Using GPU for testing')
+
     print('model_path: ',model_path)
     network = MLP(num_dim, pred_dim)
-    network.load_state_dict(torch.load(model_path))
+
+    if usegpu:
+        network.load_state_dict(torch.load(model_path))
+    else:
+        network.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+
     network.eval()
 
-    validate(network, num_dim)
+    start = time.time()
+    avg_mae, pred_time = validate(network, num_dim)
+    print('avg_mae: ',avg_mae)
+    end = time.time()
+    test_secs = (time.time() - start)
+    print('Test Mean MAE for {} samples: {:.4f}'.format(num_test, avg_mae), flush=True)
+    print('Total Prediction Time elapsed to test {} samples: {:.4f} secs'.format(num_test, pred_time), flush=True)
+    print('Total Time elapsed to test {} samples: {:.4f} secs'.format(num_test, test_secs), flush=True)
 
 def print_params():
-
-    num_train = 463715
-    batch_size = 512
-    num_epochs = 102
-    num_dim = 90
-    pred_dim = 1
-    use_pca = 0
-    lr = 5e-3
-    lr_steps = [50]
-    lr_drop = 0.1
 
     print('num_train: ',num_train)
     print('batch_size: ',batch_size)
@@ -241,20 +321,25 @@ if __name__ == "__main__":
         os.makedirs(model_save_dir)
 
     num_train = 463715
+    num_test = 51630
     batch_size = 512
-    num_epochs = 102
+    num_epochs = 2
     num_dim = 90
     pred_dim = 1
     use_pca = 0
     lr = 5e-3
     lr_steps = [50]
     lr_drop = 0.1
-
+    usegpu = 1
     print_params()
 
-    #train
-    train(num_dim)
+    #train    
+
+    if usegpu:
+        train_gpu(num_dim)
+    else:
+        train_cpu(num_dim)
 
     #validate
-    # model_path = os.path.join(model_save_dir, '100.pth')
+    # model_path = os.path.join(model_save_dir, '0.pth')
     # test(model_path, num_dim)
