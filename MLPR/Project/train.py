9172002ad2ffa5616637fb959106a4f9d74e9c9b
balaji sundar <kss.balaji1@gmail.com>
2021-12-06 21:05:05 -0500

    image classification validation added

--- MLPR/Project/train.py
+++ MLPR/Project/train.py
@@ -10,6 +10,7 @@ import torch.utils.data as dataset
 from torch.utils.data import DataLoader
 from data_loader import train_data_loader
 from mobilenet import V2_m
+import cv2
 
 def get_dataloader():
 
@@ -67,10 +68,51 @@ def train():
 
         torch.save(model.state_dict(), model_save_dir + str(epoch) + '.pth')
 
+def validate(vis=0):
+
+    train_dataloader, val_dataloader = get_dataloader()
+    
+    print('model_path: ',model_path)
+    model = get_model()
+
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+    accs = []
+
+    for i, (val_input, val_label) in enumerate(val_dataloader):
+
+        val_input = val_input.cuda()
+        val_label = val_label.cuda()
+
+        output = model(val_input)
+        output = torch.max(output, 1)[1]
+
+        acc = torch.mean(torch.eq(output, val_label).float())
+
+        output = output.cpu().data.numpy()[0]
+        val_label = val_label.cpu().data.numpy()[0]
+        acc = acc.cpu().data.numpy()
+
+        val_input = val_input.cpu().data.numpy()[0]
+        val_input = np.transpose(val_input, (1, 2, 0))
+
+        accs.append(acc)
+
+        if vis:
+            print('output: ', output)
+            print('val_label: ', val_label)
+            print('acc: ', acc)
+            cv2.imshow('val_input ', val_input)
+            cv2.waitKey(-1)
+
+    mean_acc = np.mean(np.array(accs))
+    mean_acc = np.round(mean_acc, 3)
+    print('mean_acc: ', mean_acc)
+
 if __name__ == "__main__":
 
     train_data_path = '/home/balaji/Documents/code/RSL/NEU-Fall2021/MLPR/Project/state-farm-distracted-driver-detection/imgs/train/'
-    batch_size = 32
+    batch_size = 1
     num_epochs = 100
 
     lr = 1e-4
@@ -82,4 +124,11 @@ if __name__ == "__main__":
     if not os.path.exists(model_save_dir):
         os.makedirs(model_save_dir)
 
-    train()
+    train = 0
+
+    if train:
+        train()
+
+    else:
+        model_path = os.path.join(model_save_dir, '90.pth')
+        validate()
